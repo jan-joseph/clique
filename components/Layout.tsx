e032598a040564ee6121cdede2b4088aa520ab66
Jan Joseph <jan00joseph@gmail.com>
2021-12-26 19:06:17 +0530

    Layout Component Added
    Layout modified in the main app.js template

---
+++ components/Layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Header from './Header'
+
+export default function Layout({children}) {
+    return (
+        <div>
+            <Header />
+            <main>
+                {children}
+            </main>
+        </div>
+    )
+}
