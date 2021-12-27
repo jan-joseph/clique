00fcd9bb6b5d45d1d7aad0d9114958ad82e53d2c
vishal-joshy <1812-vishal@protonmail.com>
2021-12-27 20:50:48 +0530

    Update component folder structure.
    Also updated types in MainForm.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
-import Layout from "../components/Layout";
+import Layout from "../components/Layout/Layout";
 
 import "../styles/globals.css";
 
