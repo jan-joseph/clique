4efc4322eb034e7367681980050e1e1ae323669e
Jan Joseph <jan00joseph@gmail.com>
2022-01-24 13:17:06 +0530

    Added return type to Component

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { ReactElement } from "react";
 
 import Header from "./Header";
 import styles from "@styles/Home.module.css";
 
 interface Props {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
-export default function Layout({ children }: Props) {
-  return (
-    <div>
-      <Header />
-      <main className={styles.main}>{children}</main>
-    </div>
-  );
+export default function Layout({ children }: Props): ReactElement {
+	return (
+		<div>
+			<Header />
+			<main className={styles.main}>{children}</main>
+		</div>
+	);
 }
