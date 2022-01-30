80bd1f84ac8d6a07b747a72ae083dadaa304a04e
vishaljoshy18 <1812-vishal@protonmail.com>
2022-01-30 14:16:50 +0530

    Add post method for interests api

--- pages/api/interests.ts
+++ pages/api/interests.ts
@@ -1,8 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { readFromCollection } from '@firebase/fireStore';
+import { writeToDb, readFromCollection } from '@firebase/firestoreReadWrite';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const interests = await readFromCollection('interests');
-  res.status(200).json({ interests });
+  switch (req.method) {
+    case 'GET':
+      try {
+        const interests = await readFromCollection('interests');
+        res.status(200).json({ interests });
+      } catch (err) {
+        console.log(err);
+      }
+      break;
+    case 'POST':
+      try {
+        const response = await writeToDb(req.body);
+        res.status(200).json({ response });
+      } catch (err) {
+        console.log(err);
+      }
+      break;
+  }
 }
